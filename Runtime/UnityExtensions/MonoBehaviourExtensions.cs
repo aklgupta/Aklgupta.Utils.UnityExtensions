3fb9cc2eb31c7bf0d03cff30b7d394886a8c4795
Akhil Gupta <akhil.gupta.7295@gmail.com>
2025-08-23 23:27:15 +0530

    minor fix

--- Runtime/UnityExtensions/MonoBehaviourExtensions.cs
+++ Runtime/UnityExtensions/MonoBehaviourExtensions.cs
@@ -10,12 +10,15 @@ namespace Aklgupta.Utils.UnityExtensions {
 			Debug.Log($"Ext [{source.GetType().Name}] : {obj}", source as Object);
 		}
 
+		[Conditional("UNITY_EDITOR")]
 		public static void LogWarning(this object source, object obj) {
 			Debug.LogWarning($"Ext [{source.GetType().Name}] : {obj}", source as Object);
 		}
 
+		[Conditional("UNITY_EDITOR")]
 		public static void LogError(this object source, object obj) {
 			Debug.LogError($"Ext [{source.GetType().Name}] : {obj}", source as Object);
 		}
+
 	}
 }
