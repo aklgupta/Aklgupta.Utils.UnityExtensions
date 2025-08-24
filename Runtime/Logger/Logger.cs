8d03f43e4d02d7c5d7f31bfc445cf5d7b2fbfb90
Akhil Gupta <akhil.gupta.7295@gmail.com>
2025-08-24 13:13:35 +0530

    Improved Logger

--- Runtime/Logger/Logger.cs
+++ Runtime/Logger/Logger.cs
@@ -7,18 +7,22 @@ namespace Aklgupta.Utils.Logger {
 
 		[Conditional("UNITY_EDITOR")]
 		[Conditional("UNITY_LOG")]
-		public static void Log(this object source, object obj) {
-			Debug.Log($"Ext [{source.GetType().Name}] : {obj}", source as Object);
+		public static void Log(this object source, object message) {
+			Debug.Log($"{GetPrefix(source)}{message}", source as Object);
 		}
 
 		[Conditional("UNITY_EDITOR")]
-		public static void LogWarning(this object source, object obj) {
-			Debug.LogWarning($"Ext [{source.GetType().Name}] : {obj}", source as Object);
+		public static void LogWarning(this object source, object message) {
+			Debug.LogWarning($"{GetPrefix(source)}{message}", source as Object);
 		}
 
 		[Conditional("UNITY_EDITOR")]
-		public static void LogError(this object source, object obj) {
-			Debug.LogError($"Ext [{source.GetType().Name}] : {obj}", source as Object);
+		public static void LogError(this object source, object message) {
+			Debug.LogError($"{GetPrefix(source)}{message}", source as Object);
+		}
+
+		private static string GetPrefix(object source) {
+			return $"[{source.GetType().Name}] : ";
 		}
 
 	}
