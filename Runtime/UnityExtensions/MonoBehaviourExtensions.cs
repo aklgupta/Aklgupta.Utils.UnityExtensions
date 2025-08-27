a6faa9bfb75ccd0d25b420f7601bc6722a62dcff
Akhil Gupta <akhil.gupta.7295@gmail.com>
2025-08-27 21:49:41 +0530

    minor

--- Runtime/UnityExtensions/MonoBehaviourExtensions.cs
+++ Runtime/UnityExtensions/MonoBehaviourExtensions.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 namespace Aklgupta.Utils.UnityExtensions {
 	public static class MonoBehaviourExtensions {
 
+		/// <summary>
+		/// Adds the component if it is not already present, else returns the random added of type T
+		/// </summary>
+		/// <returns>true if new component is added, false if returning an already present component</returns>
 		public static bool TryAddComponent<T>(this MonoBehaviour behaviour, out T component) where T : Component {
 			if (behaviour.TryGetComponent(out component))
 				return false;
