2ca8e86c9eb17f577dca02faf2d313f08642a69f
Akhil Gupta <akhil.gupta.7295@gmail.com>
2026-03-27 21:57:53 +0530

    minor fix
    Signed-off-by: Akhil Gupta <akhil.gupta.7295@gmail.com>

--- Runtime/UnityExtensions/MonoBehaviourExtensions.cs
+++ Runtime/UnityExtensions/MonoBehaviourExtensions.cs
@@ -4,7 +4,7 @@ namespace Aklgupta.Utils.UnityExtensions {
 	public static class MonoBehaviourExtensions {
 
 		/// <summary>
-		/// Adds the component if it is not already present, else returns the random added of type T
+		/// Adds the component if it is not already present, else returns the already added component of type T
 		/// </summary>
 		/// <returns>true if new component is added, false if returning an already present component</returns>
 		public static bool TryAddComponent<T>(this MonoBehaviour behaviour, out T component) where T : Component {
